3961f053217ad91ac7f21e10bb11461c1e44dd45
Josue <josuepariona01@gmail.com>
2023-07-18 19:48:14 -0500

    BackendE01P1

---
+++ src/main/java/com/apirest/pe/SpringAngularApiApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularApiApplication.class, args);
+	}
+
+}
